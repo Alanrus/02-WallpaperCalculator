210d2814b696b5ec9eb5189497d2ed9d1ebd3047
Aleksandr Andreev <alan.tat@ya.ru>
2019-09-19 17:04:32 +0300

    fixes #2, fixes #3, fixes #4

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
         double roomWidthInMeters = 6;
         double roomPerimeter = (2 * roomLengthInMeters) + (2 * roomWidthInMeters);
         double rollWidthInMeters = 1.06;
-        double rollQuantity = roomPerimeter / rollWidthInMeters;
-        rollQuantity = Math.round(rollQuantity);
+        double rollQuantity = Math.round(roomPerimeter / rollWidthInMeters);
         double rollLengthInMeters = 10;
-        double roomHeightInMeters = 2.75; // in meters
-        roomHeightInMeters = roomHeightInMeters + 0.1;
+        double increaseRoomHeightInMeters = 0.1;
+        double roomHeightInMeters = 2.75;
+        roomHeightInMeters = roomHeightInMeters + increaseRoomHeightInMeters;
         rollQuantity = rollQuantity / Math.floor((rollLengthInMeters / roomHeightInMeters));
         System.out.println("Вам потребуется " + Math.round(rollQuantity) + " рулонов шириной " + rollWidthInMeters + " м и длиной " + rollLengthInMeters + " м");
         System.out.println("Рекомендуем приобрести дополнительно 1-2 дополнительных рулона в запасе");
